f5ae779aaa21a2939569e1f645492303cefc2bcc
eldor <mr.eldorabdukhamidov@gmail.com>
2018-12-01 08:54:04 +0500

    Third commit

--- app/src/main/java/com/eldor/hitorch/MainActivity.java
+++ app/src/main/java/com/eldor/hitorch/MainActivity.java
@@ -5,13 +5,18 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.eldor.hitorch.fragment.DashboardFragment;
 import com.eldor.hitorch.fragment.ReviewFragment;
 import com.eldor.hitorch.fragment.HomeFragment;
 import com.eldor.hitorch.fragment.NotificationFragment;
 import com.eldor.hitorch.fragment.ProfileFragment;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
 
@@ -25,7 +30,9 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         BottomNavigationView navigationView = findViewById(R.id.navigation);
         navigationView.setOnNavigationItemSelectedListener(this);
 
+
     }
+
 
     private boolean loadfragment(Fragment fragment) {
         if(fragment!=null){
--- app/src/main/java/com/eldor/hitorch/fragment/HomeFragment.java
+++ app/src/main/java/com/eldor/hitorch/fragment/HomeFragment.java
@@ -1,25 +1,47 @@
 package com.eldor.hitorch.fragment;
 
+import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.TextView;
 
 import com.eldor.hitorch.R;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements TextView.OnEditorActionListener {
 
+
+    private AutoCompleteTextView textView = null;
+    private LinearLayoutManager layoutManager = null;
+
+    private ArrayAdapter<String> autocompletetexts = null;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
-    }
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        textView = view.findViewById(R.id.find_place);
+        textView.setCursorVisible(false);
+        autocompletetexts = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_dropdown_item, getResources().getStringArray(R.array.regions));
+        textView.setThreshold(1);
+        textView.setAdapter(autocompletetexts);
+        textView.setOnEditorActionListener(this);
 
+        //hidekeyboard();
+
+        return view;
+    }
 
     @Override
     public void onAttach(Context context) {
@@ -31,5 +53,21 @@ public class HomeFragment extends Fragment {
     public void onDetach() {
         super.onDetach();
     }
+
+
+    @Override
+    public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+        if(i == EditorInfo.IME_ACTION_GO){
+            hidekeyboard();
+        }
+        return false;
+    }
+
+    private void hidekeyboard() {
+        InputMethodManager imm = (InputMethodManager)getActivity()
+                .getSystemService(Activity.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(getActivity().getCurrentFocus().getWindowToken(),0);
+    }
+
 
 }
